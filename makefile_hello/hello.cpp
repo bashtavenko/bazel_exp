b93edd9d1b47dacd528ae6548c323aae0d147398
bashtavenko <stanbpublic@live.com>
2025-01-09 13:41:34 -0500

    Hello make example

---
+++ makefile_hello/hello.cpp
@@ -0,0 +1,6 @@
+#include "hello.h"
+#include <string>
+
+std::string Run(void) {
+    return "Hello from library!";
+}
---
+++ makefile_hello/hello.h
@@ -0,0 +1,8 @@
+#ifndef BAZEL_EXP_T_MAKEFILE_HELLO_HELLO_H_
+#define BAZEL_EXP_T_MAKEFILE_HELLO_HELLO_H_
+#include <stdio.h>
+#include <string>
+
+std::string Run(void);
+
+#endif  // BAZEL_EXP_T_MAKEFILE_HELLO_HELLO_H_
